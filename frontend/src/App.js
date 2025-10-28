5455e13f5f7529d8f2ebec33f507bd693568f52b
firoskk <firoskk@gmail.com>
2025-10-28 18:40:08 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,15 @@ import logo from './logo.png';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import FootballFixtures from './footballFixtures';
 import BadmintonFixtures from './badmintonFixtures';
-import './App.css';
+//import './App.css';
+
 <img src={logo} alt="App Logo" />
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1 style={{ textAlign: "center" }}>Meraki25 Schedule</h1>
+        <h1 style={{ textAlign: "left" }}>Meraki25 Schedule</h1>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/football" element={<FootballFixtures />} />
@@ -25,7 +26,7 @@ function LandingPage() {
   return (
     <div className="landing">
       <img src="/logo.png" alt="App Logo" className="logo" />
-      <h2>Select The Game</h2>
+      <h3>Select The Game</h3>
       <Link to="/football"><button>Football</button></Link>
       <Link to="/badminton"><button>Badminton</button></Link>
     </div>
